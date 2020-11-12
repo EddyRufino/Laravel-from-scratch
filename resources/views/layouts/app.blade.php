b96cd0aad2f65e156bf69b905197fc05dcf94984
Eddy Rufino <eddyjaair@gmail.com>
2020-11-11 19:17:11 -0500

    showing image user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,7 +65,13 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    <img 
+                                        src="{{ asset(Auth::user()->profile_image) }}" alt="{{ Auth::user()->name }}"
+                                        class="rounded-circle"
+                                        width="35"
+                                        height="35" 
+                                    >
+                                    <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
